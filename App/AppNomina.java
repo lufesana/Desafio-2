5bb0ba4f6a9e7680f69c4cf9b0f4b702ef1a1648
Dispensador de Agua <lufesana>
2020-11-02 16:33:19 -0500

    Se realiza Desafio numero 2...

--- App/AppNomina.java
+++ App/AppNomina.java
@@ -3,11 +3,17 @@ package com.felipe.desafio2.App;
 import java.util.ArrayList;
 import java.util.List;
 
-import com.felipe.desafio2.*;
+import com.felipe.desafio2.Directo;
+import com.felipe.desafio2.Promotor;
+import com.felipe.desafio2.Empleado;
+import com.felipe.desafio2.Vendedor;
+import com.felipe.desafio2.Freelancer;
 
 public class AppNomina {
 
     public static void main(String[] args) {
+
+//        List <Empleado> empleados =  new ArrayList<>();
 
         Promotor promotor = new Promotor("Pedro", 1200, 300, 62);
         Freelancer free = new Freelancer("Jhoana", 71000, 89);
@@ -17,7 +23,18 @@ public class AppNomina {
         Vendedor vendedorComisionista = new Vendedor("julian", 2050000, 45510000);
         Vendedor vendedorComisionista2 = new Vendedor("Carolina", 980000, 35989000);
 
+//        empleados.add(promotor);
+//        empleados.add(free);
+//        empleados.add(free2);
+//        empleados.add(fijo);
+//        empleados.add(fijo2);
+//        empleados.add(vendedorComisionista);
+//        empleados.add(vendedorComisionista2);
 
+//        empleados.forEach(empleado -> System.out.println("El salario de los empleados es : "  +  promotor.calcularSalario()));
+
+
+//        PROFE, AQUÍ ME COSTÓ COLOCARLE A CADA EMPLEADO SU RESPECTIVO SALARIO. POR TAL MOTIVO, PREFERÍ HACERLO "MANUALMENTE".
 
         System.out.println("El salario de pedro es : "  +  promotor.calcularSalario());
         System.out.println("El salario de Johana es : "  +  free.calcularSalario());
